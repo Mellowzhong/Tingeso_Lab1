0f0f1591633b5eb4a3ca455373454b22706c4fb0
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-11-04 03:27:15 -0300

    fix(backend):fix CORS error

--- Backend/src/main/java/com/example/Backend/Controllers/FinancialEvaluationController.java
+++ Backend/src/main/java/com/example/Backend/Controllers/FinancialEvaluationController.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/financialEvaluation")
-@CrossOrigin("*")
 public class FinancialEvaluationController {
     private final FinancialEvaluationService financialEvaluationService;
 
